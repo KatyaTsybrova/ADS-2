16beb1ca4ba5b27d1a2d2090c35ae47fae4b18f1
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-03-09 23:09:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -32,7 +32,7 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  double sum = x;
+  double sum = 0;
   for (uint16_t i = 2; i <= count; ++i) {
     sum += pown(-1.0, i) * calcItem(x, 2 * i + 1);
   }
@@ -40,7 +40,7 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-  double sum = 1;
+  double sum = 0.0;
   for (uint16_t i = 2; i <= count; ++i) {
     sum += pown(-1.0, i) * calcItem(x, 2 * i);
   }
