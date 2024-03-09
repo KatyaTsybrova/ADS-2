57ef20edd04994701fcf07e77eac2c9f4cc4a607
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-03-09 16:42:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,18 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
 
 double pown(double value, uint16_t n) {
-  double result = 1.0;
-  for (uint16_t i = 0; i < n; ++i) {
-    result *= value;
-  }
-  return result;
+  return std::pow(value, n);
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t result = 1;
-  for (uint16_t i = 1; i <= n; ++i) {
-    result *= i;
+  if (n == 0) {
+    return 1;
   }
-  return result;
+  return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
@@ -24,25 +20,25 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 0.0;
+  double result = 0.0;
   for (uint16_t i = 0; i < count; ++i) {
-    sum += calcItem(x, i);
+    result += calcItem(x, i);
   }
-  return sum;
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  double sum = 0.0;
+  double result = 0.0;
   for (uint16_t i = 0; i < count; ++i) {
-    sum += (i % 2 == 0 ? 1 : -1) * calcItem(x, 2 * i + 1);
+    result += (i % 2 == 0 ? 1 : -1) * calcItem(x, 2 * i + 1);
   }
-  return sum;
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  double sum = 0.0;
+  double result = 0.0;
   for (uint16_t i = 0; i < count; ++i) {
-    sum += (i % 2 == 0 ? 1 : -1) * calcItem(x, 2 * i);
+    result += (i % 2 == 0 ? 1 : -1) * calcItem(x, 2 * i);
   }
-  return sum;
+  return result;
 }
